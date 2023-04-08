21150852ddb1c3a920b847d6675bbdea4287176f
a.seif <a.seif@cirrus-me.com>
2023-04-08 12:55:08 +0300

    Remove next/font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
-import { Inter } from 'next/font/google'
+// import { Inter } from 'next/font/google'
 import IntroSection from '../components/intro-section/intro-section'
 import About from '../components/about/about'
 import Services from '../components/services/services'
 import ContactUs from '../components/contact-us/contact-us'
 
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
