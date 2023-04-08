4d843191d129a41424f80e2e3d9a37d3bb9991ab
a.seif <a.seif@cirrus-me.com>
2023-04-08 12:51:44 +0300

    Fix some issues in app.tsx and documents.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          {/* <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Montserrat:300,400,500,700&display=optional" rel="stylesheet" /> */}
+          <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Montserrat:300,400,500,700&display=optional" rel="stylesheet" />
         </Head>
         <body>
           <Main />
